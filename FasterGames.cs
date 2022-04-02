76be57eaafb09b583331cd404e02179f90719cee
Jin Azler <jin.azler@gmail.com>
2022-04-02 04:00:54 -0300

    Fixed for Survivors of the Void
    Fixed and added capitalizations so visual studio likes the code more

--- FasterGames.cs
+++ FasterGames.cs
@@ -14,30 +14,30 @@ namespace FasterGames
 {
     [R2APISubmoduleDependency("DifficultyAPI")]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.CwakrJax.FasterGames", "FasterGames", "1.0.0")]
+    [BepInPlugin("com.CwakrJax.FasterGames", "FasterGames", "1.0.1")]
     public class FasterGames : BaseUnityPlugin
     {
 
         public static ConfigEntry<bool> IsModEnabled { get; set; }
-        public static ConfigEntry<float> spawnRate { get; set; }
+        public static ConfigEntry<float> SpawnRate { get; set; }
 
-        public static ConfigEntry<float> baseExpMultiplier { get; set; }
-        public static ConfigEntry<float> expPerPlayerMultiplier { get; set; }
+        public static ConfigEntry<float> BaseExpMultiplier { get; set; }
+        public static ConfigEntry<float> ExpPerPlayerMultiplier { get; set; }
 
-        public static ConfigEntry<float> baseMoney { get; set; }
-        public static ConfigEntry<float> moneyPerPlayer { get; set; }
+        public static ConfigEntry<float> BaseMoney { get; set; }
+        public static ConfigEntry<float> MoneyPerPlayer { get; set; }
 
-        public static ConfigEntry<float> moveSpeed { get; set; }
+        public static ConfigEntry<float> MoveSpeed { get; set; }
 
-        public static ConfigEntry<float> scalingPercentage { get; set; }
+        public static ConfigEntry<float> ScalingPercentage { get; set; }
 
-        public static ConfigEntry<float> baseInteractableMultiplier { get; set; }
-        public static ConfigEntry<float> perPlayerInteractableMultiplier { get; set; }
+        public static ConfigEntry<float> BaseInteractableMultiplier { get; set; }
+        public static ConfigEntry<float> PerPlayerInteractableMultiplier { get; set; }
 
-        public static ConfigEntry<int> chanceShrineItemCount { get; set; }
-        public static ConfigEntry<float> chanceShrineCostMultiplier { get; set; }
+        public static ConfigEntry<int> ChanceShrineItemCount { get; set; }
+        public static ConfigEntry<float> ChanceShrineCostMultiplier { get; set; }
 
-        public static ConfigEntry<float> teleporterChargeMultiplier { get; set; }
+        public static ConfigEntry<float> TeleporterChargeMultiplier { get; set; }
 
         [System.Obsolete]
         public void Awake()
@@ -60,7 +60,7 @@ namespace FasterGames
             Color DifficultyColor = new Color(0.875f, 0.875f, 0.14f);
 
             DifficultyDef FasterDifficulty = new DifficultyDef(
-                (scalingPercentage.Value - 1) * 5, //0 is Normal mode. 2.5f is 50% which is monsoon
+                (ScalingPercentage.Value - 1) * 5, //0 is Normal mode. 2.5f is 50% which is monsoon
                 "Faster",
                 "",
                 GenerateDifficultyDescription(),
@@ -78,12 +78,12 @@ namespace FasterGames
                     Hooks myHooks = new Hooks();
                     myHooks.pluginLogger = Logger;
                     myHooks.IncreaseSpawnRate();
-                    myHooks.IncreaseExpCoefficient(baseExpMultiplier.Value, expPerPlayerMultiplier.Value);
-                    myHooks.IncreaseMoneyMultiplier(baseMoney.Value, moneyPerPlayer.Value);
-                    myHooks.IncreaseBaseStats(moveSpeed.Value);
-                    myHooks.IncreaseChestSpawnRate(baseInteractableMultiplier.Value, perPlayerInteractableMultiplier.Value);
-                    myHooks.OverhaulChanceShrines(chanceShrineItemCount.Value, chanceShrineCostMultiplier.Value);
-                    myHooks.IncreaseTeleporterChargeSpeed(teleporterChargeMultiplier.Value);
+                    myHooks.IncreaseExpCoefficient(BaseExpMultiplier.Value, ExpPerPlayerMultiplier.Value);
+                    myHooks.IncreaseMoneyMultiplier(BaseMoney.Value, MoneyPerPlayer.Value);
+                    myHooks.IncreaseBaseStats(MoveSpeed.Value);
+                    myHooks.IncreaseChestSpawnRate(BaseInteractableMultiplier.Value, PerPlayerInteractableMultiplier.Value);
+                    myHooks.OverhaulChanceShrines(ChanceShrineItemCount.Value, ChanceShrineCostMultiplier.Value);
+                    myHooks.IncreaseTeleporterChargeSpeed(TeleporterChargeMultiplier.Value);
                     myHooks.NoCoolDown3dPrinter();
                     ChatMessage.SendColored("[FasterGames] If you plan on playing different difficulty, you must restart your game!", new Color(0.78f, 0.788f, 0.3f));
                 }
@@ -99,77 +99,77 @@ namespace FasterGames
                 "Turn this off if you want to play the game normally again. \nBut why would you?"
             );
 
-            baseExpMultiplier = Config.Bind<float>(
+            BaseExpMultiplier = Config.Bind<float>(
                 "Player",
                 "baseExpMultiplier",
                 2.5f,
                 "Base Exp Gain. \nBase Game value: 1"
             );
 
-            expPerPlayerMultiplier = Config.Bind<float>(
+            ExpPerPlayerMultiplier = Config.Bind<float>(
                 "Player",
                 "expPerPlayerMultiplier",
                 0.5f,
                 "Extra Exp Per player. Helps with multiplayer scaling. \nBase Game value: 0"
             );
 
-            baseMoney = Config.Bind<float>(
+            BaseMoney = Config.Bind<float>(
                 "Player",
                 "baseMoneyMultiplier",
                 1.5f,
                 "Increases money gain for killing stuff. \nBase Game value: 1"
             );
 
-            moneyPerPlayer = Config.Bind<float>(
+            MoneyPerPlayer = Config.Bind<float>(
                 "Player",
                 "moneyPerPlayer",
                 0.1f,
                 "Increases money gain per player. Helps with multiplayer scaling. \nBase Game value: 0"
             );
 
-            moveSpeed = Config.Bind<float>(
+            MoveSpeed = Config.Bind<float>(
                 "Player",
                 "moveSpeed",
                 10f,
                 "Increases base speed so you don't move so slow. \nBase Game value: 7"
             );
 
-            scalingPercentage = Config.Bind<float>(
+            ScalingPercentage = Config.Bind<float>(
                 "Game",
                 "scalingIncreaseMultiplier",
                 2.75f,
                 "Increases game scaling. \nNormal Mode value: 1 \nMonsoon value: 1.5"
             );
 
-            baseInteractableMultiplier = Config.Bind<float>(
+            BaseInteractableMultiplier = Config.Bind<float>(
                 "Game",
                 "interactableSpawnRateMultiplier",
                 2.5f,
                 "Increases Interactable spawn Rate. \nBase Game value: 1"
             );
 
-            perPlayerInteractableMultiplier = Config.Bind<float>(
+            PerPlayerInteractableMultiplier = Config.Bind<float>(
                 "Game",
                 "interactableSpawnRatePerPlayerMultiplier",
                 0.5f,
                 "Increases Interactable spawn Rate per extra Player Past 1. \nBase Game value: 0"
             );
 
-            chanceShrineItemCount = Config.Bind<int>(
+            ChanceShrineItemCount = Config.Bind<int>(
                 "Game",
                 "chanceShrineItemAmount",
                 5,
                 "Increases amount of items you get from chance shrine before it's disabled. \nBase Game value: 2"
             );
 
-            chanceShrineCostMultiplier = Config.Bind<float>(
+            ChanceShrineCostMultiplier = Config.Bind<float>(
                 "Game",
                 "chanceShrineCostMultiplier",
                 1.2f,
                 "Increases cost of chance shrine by this amount every use (exponential).  \nBase Game value: 1.4"
             );
 
-            teleporterChargeMultiplier = Config.Bind<float>(
+            TeleporterChargeMultiplier = Config.Bind<float>(
                 "Game",
                 "teleporterChargeSpeedMultiplier",
                 1.5f,
@@ -182,7 +182,7 @@ namespace FasterGames
             string desc = "Games go 3x faster. For those who love the game, but not how long it takes.\n<style=cStack>";
             desc = string.Join("\n",
                 desc,
-                $"> Difficulty Scaling: <style=cDeath>+{(scalingPercentage.Value - 1) * 100}%</style>\n",
+                $"> Difficulty Scaling: <style=cDeath>+{(ScalingPercentage.Value - 1) * 100}%</style>\n",
                 "<style=cIsHealing>+</style> Exp Rate",
                 "<style=cIsHealing>+</style> Money on Kill",
                 "<style=cIsHealing>+</style> Base Move Speed",
---
+++ releases/faster-games_1-0-1/README.md
@@ -0,0 +1,34 @@
+<img src="icon.png" align="right" height="175px" width="175px" />
+
+# RoR2 FasterGames Mod
+> RoR2 Mod which reduces game times by speeding up progression in a variety of ways
+
+This game is a blast, but in my opinion, it takes too much time to spin up. I don't have that much time in the day; thus FasterGames mod was created.
+
+The mod tinkers with increasing a few attributes to make looting quicker while increasing the difficulty scaling to compensate. The result of this is fairly fantastic making God Runs only take 30 minutes with most runs ending around the 15 minute mark instead of the 45 minute mark.
+
+Games should be about 2.5x-3x faster.
+
+Make sure you select `Faster` difficulty before playing with this mod. All members of your party must have the mod installed.
+
+
+## Game modifications
+- Spawn rate adjusted to spawn smaller mobs
+- Gain Exp Faster
+- Gain Money Faster
+- Increase base speed (For faster looting)
+- Increase Difficulty Scaling
+- Increase Chest Spawn Rates
+- Increase Teleporter Charge Rate
+- Overhauled Chance Shrine
+	- 5 items
+	- Scaling per use increased
+	- No Cooldown
+- Removes 3d printing cooldown
+
+## Recommended Mods
+`FasterGames.r2z` Added for my recommended mods to play with this mod.
+
+## Todo
+- Work on Balancing
+	- Please add an issue with you recommended balance changes!
